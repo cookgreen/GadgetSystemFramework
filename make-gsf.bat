b6d5d52a01de454829fa7eee1e3374986ef03db2
DoDoCat <dell20091234@gmail.com>
2025-07-13 22:50:22 +0800

    Update Project

--- make-gsf.bat
+++
@@ -1,8 +0,0 @@
-cd gsf_framework/
-python -m build
-
-pip uninstall gsf-framework -y
-pip install dist/gsf_framework-0.1.0-py3-none-any.whl
-
-cd..
-gsf-manager
--- run_gsf.py
+++
@@ -1,6 +0,0 @@
-from gsf_core.main_manager import GadgetManager
-
-if __name__ == '__main__':
-    print("Starting Gadget System Framework (GSF)...")
-    manager = GadgetManager()
-    manager.run()
