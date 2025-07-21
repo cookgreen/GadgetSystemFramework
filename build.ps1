d119a7ccc65bb778174919565511dd0910e910be
DoDoCat <dell20091234@gmail.com>
2025-07-21 09:29:23 +0800

    Update Build Script

--- build.ps1
+++ build.ps1
@@ -7,6 +7,7 @@ $pwd = pwd
 $sourceFile1 = Join-Path -Path $pwd -ChildPath "gsf/assets/icon.ico"
 $sourceFile2 = Join-Path -Path $pwd -ChildPath "gsf/assets/icon-install.ico"
 $destinationFolder = Join-Path -Path $pwd -ChildPath "dist/GSF_Distribution/gsf/assets/"
+$destinationServiceFolder = Join-Path -Path $pwd -ChildPath "dist/GSF_Distribution/"
 
 if (-not (Test-Path -Path $destinationFolder)) {
     Write-Host "Creating $destinationFolder"
@@ -16,13 +17,18 @@ if (-not (Test-Path -Path $destinationFolder)) {
 Copy-Item -Path $sourceFile1 -Destination $destinationFolder -Force -ErrorAction Stop
 Copy-Item -Path $sourceFile2 -Destination $destinationFolder -Force -ErrorAction Stop
 
-$serviceControlScript1 = Join-Path -Path $pwd -ChildPath "gsf/assets/icon.ico"
-$serviceControlScript2 = Join-Path -Path $pwd -ChildPath "gsf/assets/icon-install.ico"
-$serviceControlScript3 = Join-Path -Path $pwd -ChildPath "gsf/assets/icon-install.ico"
-$destinationServiceFolder = Join-Path -Path $pwd -ChildPath "dist/GSF_Distribution/"
+cd ..
+
+$pwd = pwd
+
+$serviceControlScript1 = Join-Path -Path $pwd -ChildPath "Scripts/service-install.bat"
+$serviceControlScript2 = Join-Path -Path $pwd -ChildPath "Scripts/service-remove.bat"
+$serviceControlScript3 = Join-Path -Path $pwd -ChildPath "Scripts/service-start.bat"
+$serviceControlScript4 = Join-Path -Path $pwd -ChildPath "Scripts/service-debug.bat"
 
 Copy-Item -Path $serviceControlScript1 -Destination $destinationServiceFolder -Force -ErrorAction Stop
 Copy-Item -Path $serviceControlScript2 -Destination $destinationServiceFolder -Force -ErrorAction Stop
 Copy-Item -Path $serviceControlScript3 -Destination $destinationServiceFolder -Force -ErrorAction Stop
+Copy-Item -Path $serviceControlScript4 -Destination $destinationServiceFolder -Force -ErrorAction Stop
 
 
